23470ddc968116760330d16cd3e763d6aba4f61e
youze <youzel@126.com>
2020-07-18 14:45:16 +0800

    update conf

--- command_test.go
+++ command_test.go
@@ -1,18 +1,26 @@
 package nsq
 
 import (
-	"bytes"
+	"fmt"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
 func BenchmarkCommand(b *testing.B) {
-	b.StopTimer()
-	data := make([]byte, 2048)
-	cmd := Publish("test", data)
-	var buf bytes.Buffer
-	b.StartTimer()
+	//b.StopTimer()
+	//data := make([]byte, 2048)
+	//cmd := Publish("test", data)
+	//var buf bytes.Buffer
+	//b.StartTimer()
+	//
+	//for i := 0; i < b.N; i++ {
+	//	cmd.WriteTo(&buf)
+	//}
 
 	for i := 0; i < b.N; i++ {
-		cmd.WriteTo(&buf)
+		s := strconv.Itoa(rand.Int())
+		fmt.Println(s)
 	}
+
 }
